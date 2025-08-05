9b7ada8327a7d95ca86fdc7cc2bbb42773fbc4db
RoxiAra <rociomikaelaaramayo@gmail.com>
2025-08-05 20:58:21 -0300

    base del proyecto final

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: "lightcoral" }}>
+      <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/roc%C3%ADo-mikaela-aramayo-706652324/" target="_blank">Rocio Aramayo</a></p>
+    </footer>
+  )
+}
+
+export { Footer }
