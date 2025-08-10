588fa211309cd162428bfb8bb40127ff565879c1
RoxiAra <rociomikaelaaramayo@gmail.com>
2025-08-10 00:04:43 -0300

    le di estilos a Header y Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,47 @@
+import "../styles/components/Footer.css"
+import { useAuth } from "../context/UserContext"
+import { Link } from "react-router-dom"
+
 const Footer = () => {
+
+  const { user, logout } = useAuth()
+
+  const handleLogout = () => {
+    logout()
+  }
+
+
   return (
     <footer>
-      <section>
+      <section className="footer-section">
         <h2>Contacto</h2>
 <ul>
-  <li>Email: contacto@novamarket.com</li>
-  <li>WhatsApp: +54 9 11 5555-5555</li>
-  <li>Instagram: @novamarket.ok</li>
+          <li>Email: contacto@novamarket.com</li>
+          <li>Numero: +5493885012820</li>
+  <li>Instagram: nov@market</li>
 </ul>
       </section>
-      <section>
+      <section className="footer-section3">
+        <h2>Enlaces Utiles</h2>
+        <div className="footer-link">
+          {user && (
+                <>
+                  <Link to="/">Home</Link>
+                  <Link to="/dashboard">Dashboard</Link>
+                </>
+              )}
+              {!user && (
+                <>
+                  <Link  to="/login">Login</Link>
+              <Link to="/register">Registrate</Link>
+              <Link to="/">Home</Link>
+                </>
+              )}
+        </div>
+      </section>
+      <section className="footer-section2">
         <p>Sitio desarrollado por <a href="https://www.linkedin.com/in/roc%C3%ADo-mikaela-aramayo-706652324/" target="_blank">Rocio Aramayo</a></p>
+        <p>© 2025 Nova Market.Todos los derechos reservados.</p>
       </section>
     </footer>
   )
