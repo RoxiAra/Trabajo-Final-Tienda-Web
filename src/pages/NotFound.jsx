7ab3749d1e6c38cc570252d9f4e8af24efdbc14f
RoxiAra <rociomikaelaaramayo@gmail.com>
2025-08-10 22:58:57 -0300

    le di estilos a not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,13 +1,18 @@
 import { Link } from "react-router-dom"
 import { Layout } from "../components/Layout"
+import "../styles/pages/notfound.css"
 
 const NotFound = () => {
   return (
     <Layout>
-      <h1>Página no encontrada</h1>
+      <section className="seccion-not">
+        <h1>Página no encontrada</h1>
       <p>Lo sentimos, la página que estás buscando no existe.</p>
       <p>Verificá la URL o volvé al inicio.</p>
-      <Link to="/">Ir a inicio</Link>
+        <div className="link-not">
+          <Link to="/">Ir a inicio</Link>
+      </div>
+      </section>
     </Layout>
   )
 }
