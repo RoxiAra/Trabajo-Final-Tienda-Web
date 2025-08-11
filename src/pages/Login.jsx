6f2b86c993f61b0e353544485ed85a84e054586d
RoxiAra <rociomikaelaaramayo@gmail.com>
2025-08-11 01:26:13 -0300

    cambie el nombre a Login.css

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react"
 import { Layout } from "../components/Layout"
 import { useAuth } from "../context/UserContext"
 import { useNavigate } from "react-router-dom"
-import "../styles/pages/login.css"
+import "../styles/pages/Login.css"
 
 const Login = () => {
   const [username, setUsername] = useState("")
